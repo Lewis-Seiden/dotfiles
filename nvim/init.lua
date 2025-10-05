ad31aa416ccf83230682394a8bbb38c4329ad9fb
Lewis-Seiden <lewy@seiden.us>
2025-10-05 15:47:59 -0700

    get nvim latex working

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,14 +31,24 @@ require("lazy").setup({
     { "xiyaowong/transparent.nvim", opts = {} },
     {
     	'MeanderingProgrammer/render-markdown.nvim',
-    	dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    	dependencies = { 'latex-lsp/tree-sitter-latex', 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
     	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
     	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     	---@module 'render-markdown'
     	---@type render.md.UserConfig
     	opts = {
-		render_modes = true
-	},
+		render_modes = true,
+		latex = {
+		    enabled = true,
+		    render_modes = false,
+		    converter = { 'utftex', 'latex2text' },
+		    highlight = 'RenderMarkdownMath',
+		    position = 'center',
+		    top_pad = 0,
+		    bottom_pad = 0,
+		}
+	    },
+	
     },
     { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   },
